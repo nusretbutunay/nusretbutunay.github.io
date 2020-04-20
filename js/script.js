9088574a4737cc268eec3ed909d36551200e0440
Nusret Bütünay <nusretbutunay@gmail.com>
2020-04-20 14:20:53 +0300

    Drawer check-3

--- js/script.js
+++ js/script.js
@@ -20,6 +20,7 @@ footerYear.appendChild(currentYear);
 //Drawer functions
 
 const drawer = document.querySelector(".drawer");
+const drawerLines = document.querySelectorAll(".drawer-line");
 //const navBar = document.querySelector(".nav-bar");
 const navLinks = document.querySelector(".nav-links");
 //const navItems = document.querySelectorAll(".nav-item");
@@ -31,6 +32,7 @@ function toggleNav() {
   if (!showNav) {
     drawer.classList.add("close");
     navLinks.classList.add("show");
+    drawerLines.forEach((line) => line.classList.add("close"));
     showNav = true;
   } else {
     drawer.classList.remove("close");
