05e63535f26328373c62d6dd81645c18e6a3ab18
Nusret Bütünay <nusretbutunay@gmail.com>
2020-04-20 13:06:27 +0300

    Drawer added no functionality yet

--- js/script.js
+++ js/script.js
@@ -16,3 +16,25 @@ function myFunction() {
 const currentYear = document.createTextNode(new Date().getFullYear());
 let footerYear = document.getElementById("footer-year");
 footerYear.appendChild(currentYear);
+
+//Drawer functions
+
+const drawer = document.querySelector(".drawer");
+//const navBar = document.querySelector(".nav-bar");
+const navLinks = document.querySelector(".links");
+//const navItems = document.querySelectorAll(".nav-item");
+
+let showNav = false;
+drawer.addEventListener("click", toggleNav);
+
+function toggleNav() {
+  if (!showNav) {
+    drawer.classList.add("close");
+    navLinks.classList.add("show");
+    showNav = true;
+  } else {
+    drawer.classList.remove("close");
+    navLinks.classList.remove("show");
+    showNav = false;
+  }
+}
